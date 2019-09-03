2b958d8218b4c281e8078167cbf1222c19bdc41e
Mandeep <mandeep@oaktreecloud.com>
2019-09-03 12:52:49 +0530

    Add check for account confirmation

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -3,13 +3,17 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
 
   def login
     user = User.find_by_email(params[:email])
-    if user&.valid_password?(params[:password])
-      token = JsonWebToken.encode(user_id: user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
-                     email: user.email }, status: :ok
+    if user.confirmed_at != nil
+      if user&.valid_password?(params[:password])
+        token = JsonWebToken.encode(user_id: user.id)
+        time = Time.now + 24.hours.to_i
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                       email: user.email }, status: :ok
+      else
+        render json: { error: 'unauthorized' }, status: :unauthorized
+      end
     else
-      render json: { error: 'unauthorized' }, status: :unauthorized
+      render json: { error: 'please confirm your account' }, status: :unauthorized
     end
   end
 end
