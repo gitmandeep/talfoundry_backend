24d60a9275f6d497e900597d38a279192c14d377
Mandeep <mandeep@oaktreecloud.com>
2019-09-07 14:23:14 +0530

    Update email confirmation api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,5 @@
 class Api::V1::ApiController < ApplicationController
 	skip_before_action :verify_authenticity_token
-	
-	def not_found
-    render json: { error: 'not_found' }
-  end
 
   def authorize_request
     header = request.headers['Authorization']
@@ -11,9 +7,14 @@ class Api::V1::ApiController < ApplicationController
       decoded = JsonWebToken.decode(header)
       @current_user = User.find(decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
+      render json: { errors: e.message }, status:  401
     rescue JWT::DecodeError => e
-      render json: { errors: e.message }, status: :unauthorized
+      render json: { errors: e.message }, status:  401
     end
   end
+
+  def render_error(message, status_code = 404)
+    render json: { success: false, message: message }, status: status_code
+  end
+
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,23 +6,34 @@ class Api::V1::UsersController < Api::V1::ApiController
     render json: @users, status: :ok
   end
 
+  def user_full_name
+    @user = User.find_by_email(params[:email])
+    if @user
+      render json: @user.display_full_name
+    else
+      #render json: { errors: 'Invalid email' }, status: :unauthorized
+      render_error('Invalid email', 401)
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
       render json: @user, status: :created
     else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      #render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+      render_error(@user.errors.full_messages, 422)
     end
   end
 
   def confirm_email
-    @user = User.find(params[:id])
-    if @user.confirmation_token == params[:confirmation_token]
-      @user.update_attributes(confirmed_at: Time.now, updated_at: Time.now)
-      flash.now[:notice] = 'Successfully checked in'
-      redirect_to "http://localhost:3000/login_page/mail"
+    @user = User.confirm_by_token(params[:confirmation_token])
+    if @user.errors
+      render_error(@user.errors.full_messages, 401)
+      #redirect_to "http://localhost:3000/login_page/mail"
     else
-      confirm_email_error  
+      render json: { @user, success: true, message: "Email confirmed" }, status: 200
+      #confirm_email_error  
     end
   end
 
@@ -33,7 +44,8 @@ class Api::V1::UsersController < Api::V1::ApiController
     params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :country, :role)
   end
 
-  def confirm_email_error
-    render json: { errors: 'Invalid confirmation token' }, status: :unauthorized
-  end
+  # def confirm_email_error
+  #   #render json: { errors: 'Invalid confirmation token' }, status: :unauthorized
+  #   render_error('Invalid confirmation token', 401)
+  # end
 end
