3e2faf8c984d3e17ade676a414ece832b3665f15
Mandeep <mandeep@oaktreecloud.com>
2019-09-12 11:53:42 +0530

    Add data validdations on profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ApplicationRecord
 
   belongs_to :user, :dependent => :destroy
 
-  # validates :profile_picture , :resume , :current_location_country , :current_location_city , :citizenship_country , :citizenship_full_name, presence: true
-  # validates :current_company_name , :current_job_title , :about_me , :worked_as_freelancer , :freelancing_pros_cons , presence: true
+  validates :profile_picture , :resume , :current_location_country , :current_location_city , :citizenship_country , :citizenship_full_name, presence: true
+  validates :current_company_name , :current_job_title , :about_me , :worked_as_freelancer , :freelancing_pros_cons , presence: true
 
 end
