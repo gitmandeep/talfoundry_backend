c2b2c227f3fb98f6d986a33eee1482572f271c8c
Mandeep <mandeep@oaktreecloud.com>
2019-09-12 11:55:52 +0530

    Add method for names capitalization in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :capitalize_names
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :trackable
@@ -10,8 +12,17 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :role, presence: true
   validates :email, uniqueness: true
 
+
+
+  private
+
   def display_full_name
     "#{first_name} #{last_name}"
   end
 
+  def capitalize_names
+    self.first_name = first_name.capitalize
+    self.last_name = last_name.capitalize
+  end
+  
 end
