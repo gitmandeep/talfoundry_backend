2381673bf67dc9317e22b79afe55f671ff3e3ddf
Mohan <mohanchhalotre@gmail.com>
2019-09-18 15:49:20 +0530

    quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :user_name  =>      ENV["SMTP_USER_EMAIL"],
+    :password  =>       ENV["SMTP_PASSWORD"],
+    :domain  =>         'smtp.gmail.com',
+    :address =>         'smtp.gmail.com',
+    :port =>            '587',
+    :authentication =>  :plain,
+    :enable_starttls_auto => true
+  }
 end
