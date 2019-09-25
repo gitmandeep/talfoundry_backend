77aea4d4be106adb40b11d14f0798ef0d7edce4b
Mandeep <mandeep@oaktreecloud.com>
2019-09-25 15:57:34 +0530

    Quick fix

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -1,6 +1,18 @@
 class JobSerializer < ActiveModel::Serializer
   attributes :id
-  attributes :job_title
-  attributes :job_visibility
   attributes :created_at
+	attributes :job_title
+	attributes :job_category
+	attributes :job_description
+	attributes :job_document
+	attributes :job_type
+	attributes :job_api_integration
+	attributes :job_expertise_required
+	attributes :job_visibility
+	attributes :number_of_freelancer_required
+	attributes :job_pay_type
+	attributes :job_experience_level
+	attributes :job_duration
+	attributes :job_time_requirement
 end
+
