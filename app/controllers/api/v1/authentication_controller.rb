ed1558870e0aad4499b685d1493b65c1c59c0bf8
Mandeep <mandeep@oaktreecloud.com>
2019-09-25 13:17:40 +0530

    Add profile_created attr. to token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     user = User.find_by_email(params[:email])
     if user.confirmed_at != nil
       if user.valid_password?(params[:password])
-        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role)
+        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture : "") )
         time = Time.now + 24.hours.to_i
         render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user }, status: :ok
       else
