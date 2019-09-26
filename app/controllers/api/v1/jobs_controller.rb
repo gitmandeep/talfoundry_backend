3f5a0e0c8da743edb8b73dc99a88b10ebb3461b0
Mandeep <mandeep@oaktreecloud.com>
2019-09-26 19:58:07 +0530

    Add API to create job que and job qualification

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -30,28 +30,36 @@ class Api::V1::JobsController < Api::V1::ApiController
 
 
 	def create
-    @job = @current_user.jobs.build(job_params)
-    @job_screening_questions = @job.job_screening_questions.build(job_screening_question_params)
-    @job_qualifications = @job.job_qualifications.build(job_qualification_params)
-    if @job.save
-      render :json => {success: true, message: "Job created", status: 200, job: @job}
-    else
-      render_error(@job.errors.full_messages, 422)
+    job = @current_user.jobs.build(job_params)
+    job.job_category = params[:job_category].join(",")
+    job.job_expertise_required = params[:job_expertise_required].join(",")
+    job.job_additional_expertise_required = params[:job_additional_expertise_required].join(",")
+
+    job_questions = params[:job_screening_questions]
+    job_questions.each do |job_question| 
+      job_screening_questions = job.job_screening_questions.build(:job_question => job_question)
     end
+    job_qualifications = job.job_qualifications.build(job_qualification_params)
+    
+    if job.save
+      render :json => {success: true, message: "Job created", status: 200, job: job}
+    else
+      render_error(job.errors.full_messages, 422)
+    end 
   end
 
 
 	private
 
   def job_params
-    params.permit(:job_title,:job_category,:job_description,:job_document,:job_type,:job_api_integration,:job_expertise_required,:job_visibility,:number_of_freelancer_required,:job_pay_type,:job_experience_level,:job_duration,:job_time_requirement)
+    params.permit(:job_title, {:job_category => [] } ,:job_description,:job_document,:job_type,:job_api_integration,{:job_expertise_required => []}, {:job_additional_expertise_required => []}, :job_visibility,:number_of_freelancer_required,:job_pay_type,:job_experience_level,:job_duration,:job_time_requirement, {:job_screening_questions_attributes => [:job_question]}, {:job_qualifications_attributes => {}})
   end
 
-  def job_screening_question_params
-    params.permit(:job_question_label,:job_question)
-  end
+  # def job_screening_question_params
+  #   params.permit(:job_question_label,:job_question)
+  # end
 
-   def job_qualification_params
-    params.permit(:freelancer_type,:job_success_score,:billed_on_talfoundry,:english_level,:rising_talent,:qualification_group,:location)
+  def job_qualification_params
+    params.require(:job_qualifications).permit(:freelancer_type,:job_success_score,:billed_on_talfoundry,:english_level,:rising_talent,:qualification_group,:location)
   end
 end
