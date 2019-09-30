6bb3fba58562ef93a918ed18abd275faec2a0332
Mandeep <mandeep@oaktreecloud.com>
2019-09-30 13:17:31 +0530

    Add profile id to token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     user = User.find_by_email(params[:email])
     if user.confirmed_at != nil
       if user.valid_password?(params[:password])
-        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture : "") )
+        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture : ""), profile_id: (user.profile ? user.profile.id : "")  )
         time = Time.now + 24.hours.to_i
-        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user }, status: :ok
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user, profile_id: (user.profile ? user.profile.id : "") }, status: :ok
       else
         render_error('Invalid password', 401)
       end
