446dc2eee73a9ee8c0ae98a942846a6166252008
Mandeep <mandeep@oaktreecloud.com>
2019-09-30 14:46:05 +0530

    Quick fix

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -28,18 +28,7 @@ class Api::V1::ProfilesController < Api::V1::ApiController
 
 
 	def update
-		profile = Profile.find(params[:id].to_i)
-		profile_educations = params[:profile_education]
-		profile_employments = params[:profile_employment]
-
-		profile_educations.each do |profile_education|	
-			user_education = profile.educations.build(profile_education)
-		end
-
-		profile_employments.each do |profile_employment|	
-			user_employment = profile.employments.build(profile_employment)
-		end
-
+		profile = Profile.find(params[:id])
 		if profile.update(update_profile_params)
 			render :json => {success: true, message: "Profile updated", status: 200} 
 		else
@@ -52,19 +41,16 @@ class Api::V1::ProfilesController < Api::V1::ApiController
 	private
 
 	def profile_params
-		params.permit(:profile_picture , :resume , :current_location_country , :current_location_city , :citizenship_country , :citizenship_full_name , :skype_user_name , :english_proficiency , :skill , :linkedin_profile , :github_profile , :personal_website , :development_experience , :look_for_in_company , :project_type_to_work , :project_contributed_on , :current_company_name , :current_job_title , :about_me , :worked_as_freelancer , :freelancing_pros_cons , :start_date , :current_working_hours , :working_hours_talfoundry , :engagement_time , :key_success_point , :problem_handling_strategy, { :education_attributes => []}, { :employments_attributes => []})
+		params.require(:profile).permit(:profile_picture , :resume , :current_location_country ,
+		 :current_location_city , :citizenship_country , :citizenship_full_name ,
+		  :skype_user_name , :english_proficiency , :skill , :linkedin_profile , 
+		  :github_profile , :personal_website , :development_experience ,
+		   :look_for_in_company , :project_type_to_work , :project_contributed_on , :current_company_name , :current_job_title , :about_me , :worked_as_freelancer , :freelancing_pros_cons , :start_date , :current_working_hours , :working_hours_talfoundry , :engagement_time , :key_success_point , :problem_handling_strategy)
 	end
 	
 	def update_profile_params
-		params.permit(:professional_title, :professional_overview, :youtube_video_link, :youtube_video_type, :hourly_rate, :availability)
-	end
-
-	def education_params
-		params.require(:profile_education).permit(:school, :from_date, :to_date, :degree, :area_of_study, :education_description)
-	end
-
-	def employment_params
-		params.require(:profile_employment).permit(:company_name, :country, :state, :city, :title, :role, :period_month_from, :period_year_from, :period_month_to, :period_year_to, :employment_description)
+		params.require(:profile).permit(:professional_title, :professional_overview, :youtube_video_link, :youtube_video_type, :hourly_rate, :availability, 
+			educations_attributes: [:school, :from_date, :to_date, :degree, :area_of_study, :education_description], employments_attributes: [:company_name, :country, :state, :city, :title, :role, :period_month_from, :period_year_from, :period_month_to, :period_year_to, :employment_description])
 	end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module TalfoundryBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :put, :patch]
       end
     end
 
