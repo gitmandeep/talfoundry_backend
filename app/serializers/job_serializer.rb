5193775598dd44cfc4f5ea4c2ed6917df1fd4668
Mandeep <mandeep@oaktreecloud.com>
2019-10-01 16:13:28 +0530

    Retrun job category in a array

--- app/serializers/job_serializer.rb
+++ app/serializers/job_serializer.rb
@@ -14,8 +14,25 @@ class JobSerializer < ActiveModel::Serializer
 	attributes :job_experience_level
 	attributes :job_duration
 	attributes :job_time_requirement
+	attributes :job_additional_expertise_required
 
 	has_many :job_screening_questions, serializer: JobScreeningQuestionSerializer
 	has_many :job_qualifications, serializer: JobQualificationSerializer
+
+
+	def job_category
+		object.job_category.split(',')
+	end
+
+	def job_expertise_required
+		object.job_expertise_required.split(',')
+	end
+
+	def job_additional_expertise_required
+		object.job_additional_expertise_required.split(',')
+	end
+
 end
+
+
 
