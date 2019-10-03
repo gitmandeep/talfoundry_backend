3409aaad6a4a82b244f3066ce9267a69237c74b4
Mandeep <mandeep@oaktreecloud.com>
2019-10-03 16:41:31 +0530

    Add certification serializer

---
+++ app/serializers/certification_serializer.rb
@@ -0,0 +1,5 @@
+class CertificationSerializer < ActiveModel::Serializer
+  attributes :id
+  attributes :certification_name
+  attributes :certification_link
+end
--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,13 +1,19 @@
 class ProfileSerializer < ActiveModel::Serializer
   attributes :id
 	attributes :profile_picture
+	attributes :current_job_title
+	attributes :current_location_country
+	attributes :current_location_city
 	attributes :professional_title
 	attributes :professional_overview
 	attributes :skill
 	attributes :youtube_video_link
 	attributes :hourly_rate
 	attributes :availability
+	attributes :english_proficiency
 
   has_many :educations, serializer: EducationSerializer
 	has_many :employments, serializer: EmploymentSerializer
+	has_many :certifications, serializer: CertificationSerializer
+
 end
