e5921bc27b8601a03ccd7a46dd0f1fc7979c4ab8
Mandeep <mandeep@oaktreecloud.com>
2019-10-11 14:43:55 +0530

    Add user serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,20 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id
+	attributes :email
+	attributes :full_name
+	attributes :user_picture
+	attributes :profile_created
+	attributes :account_approved
+	attributes :created_at
+
+
+  # has_one :profile, serializer: ProfileSerializer
+
+  def full_name
+  	object.first_name + " " + object.last_name
+  end
+
+  def user_picture
+  	object.profile.profile_picture.try(:url)
+  end
+end
