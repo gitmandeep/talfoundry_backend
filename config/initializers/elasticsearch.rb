1f68d3defc922088919768cc56c5d9c365d1f34a
Mandeep <mandeep@oaktreecloud.com>
2019-10-16 20:31:51 +0530

    Quick fix

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  url = 'http://18.188.96.242:4000/'
+  url = 'http://localhost:9200/'
   transport_options = { request: { timeout: 250 } }
   options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
   Searchkick.client = Elasticsearch::Client.new(options)
