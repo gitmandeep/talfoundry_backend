72412bb6e72b023e6978cb6637405f18b0da56b7
Mandeep <mandeep@oaktreecloud.com>
2019-10-16 11:49:05 +0530

    Add profile uuid to token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
     user = User.find_by_email(params[:email])
     if user.confirmed_at != nil && user.account_active
       if user.valid_password?(params[:password])
-        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), profile_id: (user.profile ? user.profile.id : ""), professional_profile_created: user.professional_profile_created )
+        token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), profile_uuid: (user.profile ? user.profile.uuid : ""), professional_profile_created: user.professional_profile_created )
         time = Time.now + 24.hours.to_i
-        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user, profile_id: (user.profile ? user.profile.id : ""), profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), professional_profile_created: user.professional_profile_created }, status: :ok
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user, profile_uuid: (user.profile ? user.profile.uuid : ""), profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), professional_profile_created: user.professional_profile_created }, status: :ok
       else
         render_error('Invalid password', 401)
       end
