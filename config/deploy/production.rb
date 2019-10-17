361c3aade6b6e700d174e75ce1784471b110be67
Mandeep <mandeep@oaktreecloud.com>
2019-10-17 15:23:56 +0530

    Add new IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '18.188.96.242', user: 'deploy', roles: %w{web app db}
+server '3.19.211.224', user: 'deploy', roles: %w{web app db}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
