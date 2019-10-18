bb9ef670c7cbaf38693997893f54b4d441465d7c
Mandeep <mandeep@oaktreecloud.com>
2019-10-18 13:16:57 +0530

    Quick fix

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::AuthenticationController < Api::V1::ApiController
       if user.valid_password?(params[:password])
         token = JsonWebToken.encode(user_id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name, role: user.role, profile_created: user.profile_created, profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), profile_uuid: (user.profile ? user.profile.uuid : ""), professional_profile_created: user.professional_profile_created )
         time = Time.now + 24.hours.to_i
-        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user, profile_uuid: (user.profile ? user.profile.uuid : ""), profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), professional_profile_created: user.professional_profile_created }, status: :ok
+        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),email: user.email, user: user, profile_uuid: (user.profile ? user.profile.uuid : ""), profile_picture: (user.profile ? user.profile.profile_picture.try(:url) : ""), professional_profile_created: user.professional_profile_created, user_job: (user.jobs ? true : false) }, status: :ok
       else
         render_error('Invalid password', 401)
       end
