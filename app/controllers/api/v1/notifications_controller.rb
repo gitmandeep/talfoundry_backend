02d7c16eb582041fe611c56ca7b4af9e6129c0c7
ayushi rathore <ayushi@oaktreecloud.com>
2019-11-11 14:42:56 +0530

    Changes in Invite show API

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::NotificationsController < Api::V1::ApiController
   before_action :authorize_request
 
   def index
-  	notifications = @current_user.notifications
-  	notifications.present? ? (render json: notifications, each_serializer: NotificationSerializer) : (render json: { error: 'No notifications' }, status: 404)
+  	notifications = notifications.present? ? @current_user.notifications : []
+  	render json: notifications, each_serializer: NotificationSerializer, status: :ok
   end
 
 end
