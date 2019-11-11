a53d687849b0d74c416ac345006b6e1635b35b58
ayushi rathore <ayushi@oaktreecloud.com>
2019-11-11 20:00:11 +0530

    Bug fix

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::NotificationsController < Api::V1::ApiController
   before_action :authorize_request
 
   def index
-  	notifications = notifications.present? ? @current_user.notifications : []
+  	notifications = @current_user.notifications.present? ? @current_user.notifications : []
   	render json: notifications, each_serializer: NotificationSerializer, status: :ok
   end
 
