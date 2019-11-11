4b1e68a4d627b2fb5660dd871ec4f82696afb47c
ayushi rathore <ayushi@oaktreecloud.com>
2019-11-11 18:42:23 +0530

    Changes to document uploader

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -1,5 +1,8 @@
 class JobApplication < ApplicationRecord
+	mount_base64_uploader :document, JobApplicationDocumentUploader
+
 	belongs_to :user
 	belongs_to :job
 	has_many :job_application_answers, :dependent => :destroy
+	accepts_nested_attributes_for :job_application_answers
 end
