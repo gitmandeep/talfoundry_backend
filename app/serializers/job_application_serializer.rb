45ab522e52e3276698fc520a72274466cec28f50
Mandeep <mandeep@oaktreecloud.com>
2019-11-11 19:28:05 +0530

    Quick fix

--- app/serializers/job_application_serializer.rb
+++ app/serializers/job_application_serializer.rb
@@ -1,8 +1,12 @@
 class JobApplicationSerializer < ActiveModel::Serializer
-	attributes :id, :uuid, :job_id, :cover_letter, :job_application_document, :price
-	has_many :job_application_answers, serializer: JobApplicationAnswerSerializer
+	attributes :id, :uuid, :job_id, :cover_letter, :job_application_document, :price, :job_aplication_answer
+	#has_many :job_application_answers, serializer: JobApplicationAnswerSerializer
 
 	def job_application_document
 		object.document.try(:url)
+	end
+
+	def job_aplication_answer
+		object.job_application_answers
 	end
 end
