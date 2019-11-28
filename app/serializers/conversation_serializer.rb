56fbdb8173a4e94b4f517a843603c8a188b1ee33
Mandeep <mandeep@oaktreecloud.com>
2019-11-28 11:27:38 +0530

    Quick fix

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -1,7 +1,13 @@
 class ConversationSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id
+  attributes :uuid
+  attributes :title 
+  attribute :created_at
   attribute :sender
+  attribute :sender_picture
   attribute :receiver
+  attribute :receiver_picture
+
   has_many :messages
 
   def sender
@@ -10,5 +16,13 @@ class ConversationSerializer < ActiveModel::Serializer
 
   def receiver
 	  @object.recipient
+  end
+
+  def sender_picture
+    @object.sender.profile ? @object.sender.profile.try(:profile_picture).try(:url)  : ''
+  end
+
+  def receiver_picture
+    @object.recipient.profile ? @object.recipient.profile.try(:profile_picture).try(:url)  : ''
   end
 end
