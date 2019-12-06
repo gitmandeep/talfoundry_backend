775b2030f5a81c2360d9cee450a11f601e396bb8
ayushi rathore <ayushi@oaktreecloud.com>
2019-12-06 17:40:03 +0530

    Modification to conversation serializer

--- app/serializers/conversation_serializer.rb
+++ app/serializers/conversation_serializer.rb
@@ -4,25 +4,29 @@ class ConversationSerializer < ActiveModel::Serializer
   attribute :title 
   attribute :created_at
   attribute :sender
-  attribute :sender_picture
+  #attribute :sender_picture
   attribute :receiver
-  attribute :receiver_picture
+  #attribute :receiver_picture
 
   has_many :messages
 
   def sender
-  	@object.sender
+  	@sender = @object.sender
+    @sender.user_name = @sender.image.try(:url)
+    return @sender
   end
 
   def receiver
-	  @object.recipient
+    @receiver = @object.recipient
+    @receiver.user_name = @receiver.profile ? @receiver.profile.try(:profile_picture).try(:url)  : ''
+    return @receiver
   end
 
-  def sender_picture
-    @object.sender.profile ? @object.sender.profile.try(:profile_picture).try(:url)  : ''
-  end
+  # def sender_picture
+  #   @object.sender.profile ? @object.sender.profile.try(:profile_picture).try(:url)  : ''
+  # end
 
-  def receiver_picture
-    @object.recipient.profile ? @object.recipient.profile.try(:profile_picture).try(:url)  : ''
-  end
+  # def receiver_picture
+  #   @object.recipient.profile ? @object.recipient.profile.try(:profile_picture).try(:url)  : ''
+  # end
 end
