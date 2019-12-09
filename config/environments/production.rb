891c1584f3396257fb01ad2ab08757ad399988c1
Mandeep <mandeep@oaktreecloud.com>
2019-12-09 17:10:40 +0530

    Quick fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  # config.action_cable.url = [/ws:\/\/*/, /wss:\/\/*/]
+  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -47,7 +49,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
