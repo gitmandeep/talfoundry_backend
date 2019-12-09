4da0cd4530626112355ad703209f01205ec82536
Mandeep <mandeep@oaktreecloud.com>
2019-12-09 17:51:15 +0530

    Add pagination on jobs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
 	#before_action :authenticate_user!
 	#skip_before_action :verify_authenticity_token
 	protect_from_forgery with: :null_session
+	include Rails::Pagination
 end
