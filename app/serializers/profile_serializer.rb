9a9dbb9e576132b467ee06e3e6030631733b7b1c
Mandeep <mandeep@oaktreecloud.com>
2019-12-20 12:58:20 +0530

    Add is_certified attr.

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :uuid, :user_id, :user_uuid, :profile_type, :profile_picture, :current_job_title, :current_location_country, :current_location_city, :professional_title, :professional_overview, :category, :skill, :user_certification, :youtube_video_link, :hourly_rate, :availability, :visibility, :english_proficiency, :about_me, :development_experience, :experience_level, :search_engine_privacy, :project_preference, :earnings_privacy, :name, :available_jobs, :available_jobs_for_contract, :search_keywords, :favorited_freelancer, :invites_count, :contracts_count, :invite_id, :contract_id, :profile_strength
+  attributes :id, :uuid, :user_id, :user_uuid, :profile_type, :profile_picture, :current_job_title, :current_location_country, :current_location_city, :professional_title, :professional_overview, :category, :skill, :user_certification, :youtube_video_link, :hourly_rate, :availability, :visibility, :english_proficiency, :about_me, :development_experience, :experience_level, :search_engine_privacy, :project_preference, :earnings_privacy, :name, :available_jobs, :available_jobs_for_contract, :search_keywords, :favorited_freelancer, :invites_count, :contracts_count, :invite_id, :contract_id, :profile_strength, :is_certified
   #attributes :profile_picture_base64
 
   has_many :educations, serializer: EducationSerializer
@@ -28,6 +28,10 @@ class ProfileSerializer < ActiveModel::Serializer
 
   def user_certification
   	object.certification.try(:split, (','))
+  end
+
+  def is_certified
+    object.certifications.present?
   end
 
   def name
