be13d7c2b6c0c0375f8f08a6049029a550ec2e95
ayushi rathore <ayushi@oaktreecloud.com>
2020-01-29 20:23:01 +0530

    Parameter changes

--- app/controllers/api/v1/phone_verifications_controller.rb
+++ app/controllers/api/v1/phone_verifications_controller.rb
@@ -1,18 +1,17 @@
-class PhoneVerificationsController < ApplicationController
-  skip_before_action :verify_authenticity_token
+class Api::V1::PhoneVerificationsController < Api::V1::ApiController
 
   def verify_from_message
     user = get_user_for_phone_verification
     user.mark_phone_as_verified! if user
 
-    render nothing: true
+    render json: "Phone number verified", success: true, status: 200
   end
 
   private
 
   def get_user_for_phone_verification
-    phone_verification_code = params['Body'].try(:strip)
-    phone_number            = params['From'].gsub('+1', '')
+    phone_verification_code = params['otp'].try(:strip)
+    phone_number            = params['phone'].gsub('+1', '')
 
     condition = { phone_verification_code: phone_verification_code,
                   phone_number: phone_number }
