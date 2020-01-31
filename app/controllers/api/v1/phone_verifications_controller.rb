dade9eaefb0f9240207c4c05ed82c8a3403f74ec
ayushi rathore <ayushi@oaktreecloud.com>
2020-01-31 20:31:24 +0530

    bug fix

--- app/controllers/api/v1/phone_verifications_controller.rb
+++ app/controllers/api/v1/phone_verifications_controller.rb
@@ -17,7 +17,7 @@ class Api::V1::PhoneVerificationsController < Api::V1::ApiController
     if message.send_sms
       render json: "Phone number verified", success: true, status: 200
     else
-      render json: {message: "#{message.error}", status: 401}
+      render_error("#{messag.error}", 401)
     end
   end
 
